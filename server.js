4f2b1817a8ef771826745228237ec8ab87a57785
Bishop <lake.d.bishop@gmail.com>
2019-09-09 15:44:15 -0500

    all endpoints complete MVP

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ server.get('/api/accounts', (req, res) => {
     db('accounts')
         .then(accounts => {
             res.status(200).json(accounts)
-            console.log(accounts)
         })
         .catch(err => {
             res.status(500).json({error: 'there was an issue getting the accounts from the database'})
@@ -36,6 +35,26 @@ server.post('/api/accounts', (req, res) => {
         })
         .catch(err => {
             res.status(500).json({error: 'there was an issue createing the account in the database'})
+        })
+})
+
+server.put('/api/accounts/:id', (req, res) => {
+    db('accounts').where({id: req.params.id}).update({name: req.body.name, budget: req.body.budget})
+        .then(num => {
+            res.status(200).json(num)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'there was an issue updateing the record in the database'})
+        })
+}) 
+
+server.delete("/api/accounts/:id", (req, res) => {
+    db('accounts').where({id: req.params.id}).del()
+        .then(num => {
+            res.status(200).json(num)
+        })
+        .catch(err => {
+            res.status(500).json({error: 'there was an issue deleteing the record from the server'})
         })
 })
 
